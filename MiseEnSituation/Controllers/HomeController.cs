d421749d3385fd085b0ea4ece4d4d43545165df9
Crowfall <nicolaspotier1@gmail.com>
2020-12-15 14:14:02 +0100

    modif checklogin / menu

--- MiseEnSituation/Controllers/HomeController.cs
+++ MiseEnSituation/Controllers/HomeController.cs
@@ -70,29 +70,37 @@ namespace MiseEnSituation.Controllers
             //utilisiation du IUserService
             try
             {
-                User foundedUser = _userService.CheckLogin(u.Email, u.Password, u.Type);
-                if (foundedUser != null && u.Type == UserType.ADMIN)
-                {
-                    Session["user_id"] = foundedUser.Id;
-                    Session["user_connected"] = true;
-                    //ViewBag.UserType = UserType.ADMIN;
-                    return RedirectToAction("Index", "Admin"); // /Admin/Index
-                    
+
+                User fu = _userService.CheckLogin(u.Email, u.Password, u.Type);
+                        
+
+                    if (fu != null && fu.Type.ToString().Equals(UserType.ADMIN.ToString()))
+                    {
+                        Session["user_id"] = fu.Id;
+                        Session["user_connected"] = true;
+                        return RedirectToAction("Index", "Admin"); // /Admin/Index             
+                    }
+
+                    else if(fu != null && fu.Type == UserType.MANAGER)
+                    {
+                        Session["user_id"] = fu.Id;
+                        Session["user_connected"] = true;
+                        return RedirectToAction("Index", "Manager"); // /Manager/Index             
+                    }
+
+                    else if (fu != null && fu.Type == UserType.EMPLOYEE)
+                    {
+                        Session["user_id"] = fu.Id;
+                        Session["user_connected"] = true;
+                        return RedirectToAction("Index", "Employee"); // /Employee/Index    
+                }
+                    else if (fu != null && fu.Type == UserType.RH)
+                    {
+                        Session["user_id"] = fu.Id;
+                        Session["user_connected"] = true;
+                        return RedirectToAction("Index", "RH"); // /RH/Index    
                 }
 
-                //else
-                //{
-                //    u.Type.Equals(UserType.EMPLOYEE);
-                //    ViewBag.UserType = UserType.EMPLOYEE;
-                //    return RedirectToAction("Index", "Employee"); // /Employee/Index
-                //    u.Type.Equals(UserType.RH);
-                //    ViewBag.UserType = UserType.RH;
-                //    return RedirectToAction("Index", "RH"); // /RH/Index
-                //    u.Type.Equals(UserType.MANGAGER);
-                //    ViewBag.UserType = UserType.MANGAGER;
-                //    return RedirectToAction("Index", "Manager"); // /Manager/Index
-
-                //}
             }
             catch (Exception ex)
             {
