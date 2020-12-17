e2144ec96b4859944418b4f8076f1b8aa18a1f22
SylvainJanet <74711059+SylvainJanet@users.noreply.github.com>
2020-12-17 17:54:01 +0100

    Addresses

---
+++ MiseEnSituation/Repositories/AddressRepository.cs
@@ -0,0 +1,17 @@
+﻿using GenericRepositoryAndService.Repository;
+using MiseEnSituation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiseEnSituation.Repositories
+{
+    public class AddressRepository : GenericRepository<Address> , IAddressRepository
+    {
+        public AddressRepository(MyDbContext db) : base(db)
+        {
+
+        }
+    }
+}
---
+++ MiseEnSituation/Services/IAddressService.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepositoryAndService.Service;
+using MiseEnSituation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiseEnSituation.Services
+{
+    public interface IAddressService : IGenericService<Address>
+    {
+    }
+}
