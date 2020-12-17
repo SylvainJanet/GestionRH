3f8b9ebdc5f4c37aefa571884482f685a1c8e930
SylvainJanet <74711059+SylvainJanet@users.noreply.github.com>
2020-12-17 11:17:41 +0100

    Added display names Error handling in forms Added unitary test project

--- MiseEnSituation/Models/TrainingCourse.cs
+++ MiseEnSituation/Models/TrainingCourse.cs
@@ -9,30 +9,40 @@ namespace MiseEnSituation.Models
 {
     public class TrainingCourse : BaseEntity
     {
-        [Required]
+        [Required(ErrorMessage = "Name is required")]
         [MaxLength(200)]
+        [Display(Name = "Name")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Starting date is required")]
         [DataType(DataType.Date)]
+        [Display(Name = "Starting date")]
         public DateTime StartingDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ending date is required")]
         [DataType(DataType.Date)]
+        [Display(Name = "Ending date")]
         public DateTime EndingDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Duration in hours is required")]
+        [Display(Name = "Duration in hours")]
         public double DurationInHours { get; set; }
 
+        [Display(Name = "Price")]
+        [DisplayFormat(DataFormatString = "{0:#.##}")]
         public double? Price { get; set; }
 
+        [Display(Name = "Enrolled employees")]
         public IList<Employee> EnrolledEmployees { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Trained skills is required")]
+        [Display(Name = "Trained skills")]
         public IList<Skill> TrainedSkills { get; set; }
 
+        [Display(Name = "Reports finished")]
         public IList<CheckUpReport> ReportsFinished { get; set; }
 
+        [Display(Name = "Reports wished")]
         public IList<CheckUpReport> ReportsWished { get; set; }
 
         public TrainingCourse(string name, DateTime startingDate, DateTime endingDate, double durationInHours, List<Skill> trainedSkills) : this()
